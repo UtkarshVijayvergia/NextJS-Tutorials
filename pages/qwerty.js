0f38398eb7b37b8bd0beeeceb6300f02e50d37ec
Utkarsh <utkarsh.v1901@gmail.com>
2022-06-20 20:12:17 +0530

    SSR

---
+++ pages/qwerty.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const qwerty = () => {
+  return (
+    <div>qwerty</div>
+  )
+}
+
+export default qwerty
---
+++ pages/qwerty/qwerty.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const qwerty = () => {
+  return (
+    <div>qwerty2</div>
+  )
+}
+
+export default qwerty
